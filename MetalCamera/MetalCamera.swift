cf8b7cc7b034d3d926268e06b7b4809271c5c998
Boris Dering <borisdering@Boriss-MBP.fritz.box>
2020-04-13 12:01:00 +0200

    added camera session implementation

--- MetalCamera/MetalCamera.swift
+++ MetalCamera/MetalCamera.swift
@@ -9,14 +9,28 @@
 import UIKit
 import AVFoundation
 
+/// This protocol is used to log every peace that goes on in this libary
+/// so you are free to use this protocol to be able to use
+/// your own logger implementation 
+public protocol MetalCameraLogger {
+    func info(_ message: String)
+    func warning(_ message: String)
+    func error(_ message: String)
+}
+
 open class MetalCameraViewController: UIViewController {
     
-    var captureSession: AVCaptureSession!
-    var queue: DispatchQueue!
+    /// Dispatch queue to use to avoid freezing main thred
+    open var queue = DispatchQueue(label: "video_capture_queue")
+    
+    var captureSession: AVCaptureSession?
+    
+    open var logger: MetalCameraLogger?
     
     override open func viewDidLoad() {
         super.viewDidLoad()
         
+        // check for authorization status and request permissions if needed
         if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) == .authorized {
             self.setupCamera()
         } else {
@@ -28,23 +42,82 @@ open class MetalCameraViewController: UIViewController {
 
     open func setupCamera() {
         
+        self.queue.async { [weak self] in
+            
+            self?.createCaptureSession()
+            
+            do {
+                guard let device = try self?.videoDevice(for: AVCaptureDevice.Position.back) else { return }
+                
+                guard let input = try self?.createDeviceInput(for: device) else { return }
+                
+                try self?.addInput(input)
+                
+                guard let output = self?.createCaptureOutput() else { return }
+                
+                try self?.addOutput(output)
+                
+                try self?.setupPreview()
+                
+                self?.startRunning()
+            } catch {
+                self?.logger?.error(error.localizedDescription)
+            }
+        }
+    }
+    
+    open func createCaptureSession() {
         self.captureSession = AVCaptureSession()
-        let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: AVCaptureDevice.Position.back)!
-        let input = try! AVCaptureDeviceInput(device: device)
+    }
+    
+    open func addOutput(_ output: AVCaptureOutput) throws {
+        guard self.captureSession?.canAddOutput(output) ?? false else { throw MetalCameraError.outputUnavailable }
+        self.captureSession?.addOutput(output)
+    }
+    
+    open func createCaptureOutput() -> AVCaptureOutput {
         
         let output = AVCaptureVideoDataOutput()
         output.alwaysDiscardsLateVideoFrames = false
-        self.queue = DispatchQueue(label: "video_capture_queue")
         output.setSampleBufferDelegate(self, queue: self.queue)
         
-        self.captureSession.addInput(input)
-        self.captureSession.addOutput(output)
+        return output
+    }
+    
+    open func videoDevice(for position: AVCaptureDevice.Position) throws -> AVCaptureDevice {
+        guard let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: position)
+            else { throw MetalCameraError.deviceNotFound }
         
-        let previewLayaer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-        previewLayaer.frame = self.view.bounds
-        self.view.layer.addSublayer(previewLayaer)
+        return device
+    }
+    
+    open func createDeviceInput(for device: AVCaptureDevice) throws -> AVCaptureDeviceInput {
+        return try AVCaptureDeviceInput(device: device)
+    }
+    
+    open func addInput(_ input: AVCaptureInput) throws {
+        guard self.captureSession?.canAddInput(input) ?? false else { throw MetalCameraError.cannotAddInput("") }
+        self.captureSession?.addInput(input)
+    }
+    
+    open func startRunning() {
+        self.captureSession?.startRunning()
+    }
+    
+    open func stopRunning() {
+        self.captureSession?.stopRunning()
+    }
+    
+    open func setupPreview() throws {
         
-        self.captureSession.startRunning()
+        guard let session = self.captureSession else { throw MetalCameraError.captureSessionUnavailable }
+        
+        let layer = AVCaptureVideoPreviewLayer(session: session)
+        
+        DispatchQueue.main.async { [weak self] in
+            layer.frame = self?.view.bounds ?? UIScreen.main.bounds
+            self?.view.layer.addSublayer(layer)
+        }
     }
     
     func authorizationStatus() -> AVAuthorizationStatus {
@@ -56,4 +129,11 @@ extension MetalCameraViewController: AVCaptureVideoDataOutputSampleBufferDelegat
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
 //        print("did output sample buffer...")
     }
+}
+
+enum MetalCameraError: Error {
+    case captureSessionUnavailable
+    case cannotAddInput(_ message: String)
+    case deviceNotFound
+    case outputUnavailable
 }
